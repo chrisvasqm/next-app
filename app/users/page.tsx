193b2e3727eb1e89870f4dfda49e38bc4dd105c4
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-03 00:13:32 -0400

    Add fetching fake users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,25 @@
 import Link from 'next/link';
 import React from 'react'
 
-const UsersPage = () => {
+interface User {
+  id: number
+  name: string
+}
+
+const UsersPage = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' });
+  const users: User[] = await response.json();
+
   return (
     <div>
-      <div>UsersPage</div>
+      <h1>Users</h1>
       <Link href={'/users/new'} className='text-blue-500'>New</Link>
+
+      <p>{new Date().toLocaleTimeString()}</p>
+
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
     </div>
   )
 }
