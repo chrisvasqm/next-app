a9bc4df90d7bd46710ed97c2e7c9e32769b65c99
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-03 09:57:45 -0400

    Add NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
 
 export default function Home() {
   return (
-    <main>
+    <div>
       <h1>Hello World</h1>
-      <Link href={'/users'} className="text-blue-500">Users</Link>
-      <ProductCard />
-    </main>
+    </div>
   )
 }
