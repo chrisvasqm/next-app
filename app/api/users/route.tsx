96cd6918bbe7dd22046cdd46b5cfe97f1b921127
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-03 14:54:10 -0400

    Refactor user POST with Prisma

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
+import schema from "./schema";
 
 export async function GET(request: NextRequest) {
     const users = await prisma.user.findMany();
@@ -10,8 +11,23 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
     const body = await request.json();
 
-    if (!body.name)
-        return NextResponse.json({ error: 'name is required' }, { status: 400 })
+    const validation = schema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors[0].message, { status: 400 })
 
-    return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+    const user = await prisma.user.findUnique({
+        where: { email: body.email }
+    })
+
+    if (user)
+        return NextResponse.json({ error: 'User already exists' }, { status: 400 })
+
+    const newUser = await prisma.user.create({
+        data: {
+            name: body.name,
+            email: body.email
+        }
+    })
+
+    return NextResponse.json(newUser, { status: 201 });
 }
