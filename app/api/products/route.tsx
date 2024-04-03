6f4dac679a4559795a3d7e046ec66af4f1fe813a
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-03 17:19:21 -0400

    Refactor product POST

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,6 +1,6 @@
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
-import prisma from "@/prisma/client";
 
 export async function GET(request: NextRequest) {
     const products = await prisma.product.findMany();
@@ -14,5 +14,16 @@ export async function POST(request: NextRequest) {
     if (!validation.success)
         return NextResponse.json(validation.error.errors[0].message, { status: 400 })
 
-    return NextResponse.json({ id: 1, name: body.name, price: body.price }, { status: 201 });
+    const product = await prisma.product.findUnique({ where: { name: body.name } })
+    if (product)
+        return NextResponse.json({ error: 'Product already exists' }, { status: 400 })
+
+    const newProduct = await prisma.product.create({
+        data: {
+            name: body.name,
+            price: body.price
+        }
+    })
+
+    return NextResponse.json(newProduct, { status: 201 });
 }
