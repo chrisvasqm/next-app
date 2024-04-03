ed218d12603fddf8e1bdfcc9cdfb6ea8049dd4ed
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-03 15:08:51 -0400

    Refactor get products endpoint

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-    return NextResponse.json([
-        { id: 1, name: 'Milk', price: 2.5 },
-        { id: 2, name: 'Bread', price: 3.5 },
-    ])
+export async function GET(request: NextRequest) {
+    const products = await prisma.product.findMany();
+
+    return NextResponse.json(products)
 }
 
 export async function POST(request: NextRequest) {
